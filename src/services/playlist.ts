9c3f5a0de92551343ef1f75840aad7fcf6b198f4
KevinU20221275 <U20221275@univo.edu.sv>
2025-04-01 17:15:15 -0600

    add try-catch to the api calls

--- src/services/playlist.ts
+++ src/services/playlist.ts
@@ -4,73 +4,103 @@ import { type ArtistPlaylist, type Playlist } from "@/data/playlist";
 
 
 export async function createPlaylist(playlistNumber:number){
-  const id = crypto.randomUUID()
-  const title = `Mi playlist n.° ${playlistNumber}`
-  const artists = ["Jhon Doe"]
-  const type = "Playlist"
-  const color = colors.gray
-  const cover = "https://cdn-icons-png.flaticon.com/512/557/557098.png"
-  const songs : number[] = []
-  const res = await fetch('/api/playlist.json', {
-        method: 'POST',
-        headers: {
-              'Content-Type': 'application/json'
-                  },
-                  body: JSON.stringify({
-                      id, title, artists, color, cover, type, songs
-                  }) 
-       
-              })
-  const data = await res.json()
+    const id = crypto.randomUUID()
+    const title = `Mi playlist n.° ${playlistNumber}`
+    const artists = ["Jhon Doe"]
+    const type = "Playlist"
+    const color = colors.green
+    const cover = "https://cdn-icons-png.flaticon.com/512/557/557098.png"
+    const songs : number[] = []
 
-  return { data : data.newPlaylist}
+    try {
+
+        const res = await fetch('/api/playlist.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id, title, artists, color, cover, type, songs
+            })
+        })
+        
+        const data = await res.json()
+        
+        return { data : data.newPlaylist}
+
+    } catch {
+        throw new Error('Error creando la playlist')
+    }
 }
 
 
 export async function addPlaylist(playlist: Playlist | Album | ArtistPlaylist){
-  const res = await fetch('/api/playlist.json', {
-                  method: 'POST',
-                  headers: {
-                      'Content-Type': 'application/json'
-                  },
-                  body: JSON.stringify(playlist) 
-       
-              })
-  const data = await res.json()
+    
+    try {
+        const res = await fetch('/api/playlist.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(playlist) 
+            
+        })
+        
+        const data = await res.json()
+        
+        return {data: data.newPlaylist}
 
-  return {data: data.newPlaylist}
+    } catch {
+        throw new Error('Error añadiendo la playlist')
+    }
 }
 
 export async function removePlaylist(id: string | number){
-  const res = await fetch(`/api/playlist.json`, {
-    method: 'DELETE',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        playlistId: id
-    }) 
-  })
-  const data = await res.json()
+    try {
+
+        const res = await fetch(`/api/playlist.json`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                playlistId: id
+            }) 
+        })
+        
+        const data = await res.json()
+
+    } catch {
+        throw new Error('Error eliminando la playlist')
+    }
 }
 
 
 export async function getMyPlaylist(id = null) {
     if (id){
-        const res = await fetch(`/api/playlist.json?id=${id}`)
-
-        if (res.ok){
-            const data = await res.json()
-            return data;
+        try {
+            const res = await fetch(`/api/playlist.json?id=${id}`)
+            
+            if (res.ok){
+                const data = await res.json()
+                return data;
+            }
+        } catch {
+            throw new Error('Error al obtener la playlist')
         }
     } else {
-        const res = await fetch('/api/playlist.json', {
-            method: 'GET',
-        })
-        if (res.ok){
-            const data = await res.json()
-    
-            return  data.myPlaylists
+        try {
+
+            const res = await fetch('/api/playlist.json', {
+                method: 'GET',
+            })
+            if (res.ok){
+                const data = await res.json()
+                
+                return  data.myPlaylists
+            }
+        } catch {
+            throw new Error('Error al obtener las playlist')
         }
     }
 }
--- src/services/song.ts
+++ src/services/song.ts
@@ -1,47 +1,59 @@
 
 
 export async function getPlaylistsSongs(id: string | number) {
-    const res = await fetch(`/api/songs.json?id=${id}`)  
-
-    if (res.ok){
-        const data = await res.json()
-        return  data
+    try {
+        const res = await fetch(`/api/songs.json?id=${id}`)  
+        
+        if (res.ok){
+            const data = await res.json()
+            return  data
+        }
+    } catch {
+        throw new Error("Error al obtener las canciones")
     }
 }
 
 export async function addSongToPlaylist(id : string | number, songId : string | number){
-    const res = await fetch('/api/songs.json', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            playlistId: id, songId
+    try {
+        const res = await fetch('/api/songs.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                playlistId: id, songId
+            })
         })
-    })
-
-    if (res.ok) {
-        return await res.json()
-    } else {
+        
+        if (res.ok) {
+            return await res.json()
+        } else {
+            throw new Error('Error al añadir la canción a la playlist')
+        }
+    } catch {
         throw new Error('Error al añadir la canción a la playlist')
     }
 }
 
 
 export async function removeSongFromPlaylist(id: string | number, songId: string | number){
-    const res = await fetch('/api/songs.json', {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            playlistId: id, songId
+    try {
+        const res = await fetch('/api/songs.json', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                playlistId: id, songId
+            })
         })
-    })
-
-    if (res.ok) {
-        return await res.json()
-    } else {
+        
+        if (res.ok) {
+            return await res.json()
+        } else {
+            throw new Error('Error al eliminar la canción de la playlist')
+        }
+    } catch {
         throw new Error('Error al eliminar la canción de la playlist')
     }
 }
