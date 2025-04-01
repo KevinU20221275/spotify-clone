43a3be0274b16ef013845c5dbbffd5bac8fe287c
KevinU20221275 <U20221275@univo.edu.sv>
2025-04-01 17:08:01 -0600

    add constants directory

---
+++ src/constants/constants.js
@@ -0,0 +1,18 @@
+export const PLAYLISTS_FILTERS = {
+    ALL: 'ALL',
+    CREATED_BY_ME: 'CREATED_BY_ME',
+    CREATED_BY_SPOTIFY: 'CREATED_BY_SPOTIFY',
+    PLAYLISTS : 'PLAYLISTS',
+    ALBUMS: 'ALBUMS',
+}
+
+export const PLAYLISTS_TYPES = {
+    PLAYLIST : "Playlist",
+    ARTIST_PLAYLIST : "Artist_Playlist",
+}
+
+export const CONTAINER_STYLE = {
+    LIST : "list",
+    COMPACT_LIST : "compact-list",
+    GRID : "grid",
+}
